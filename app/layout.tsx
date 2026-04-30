0fcb9bdc9a8ae74995dab7fc6822b817d415959e
penebangkayu <penebang.kayuu@gmail.com>
2026-04-30 10:38:10 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: "Momtana Resto",
+    default: "Momtana Resto - Ayam & bebek goreng laos spesial sambal mangga",
     template: "%s | Momtana Resto",
   },
   description:
